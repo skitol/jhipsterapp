382a7ea423b0314e549b99af9a2e5e4546e87023
Kyle Kalwarski <kkalwarski@gmail.com>
2021-01-11 22:48:04 -0500

    Initial version of findsar generated by JHipster-6.8.0 with blueprints: nodejs-1.4.0

---
+++ src/main/webapp/app/shared/layout/menus/admin.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import MenuItem from 'app/shared/layout/menus/menu-item';
+import { DropdownItem } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { NavLink as Link } from 'react-router-dom';
+import { NavDropdown } from './menu-components';
+
+const adminMenuItems = (
+  <>
+    <MenuItem icon="user" to="/admin/user-management">
+      User management
+    </MenuItem>
+    {/* jhipster-needle-add-element-to-admin-menu - JHipster will add entities to the admin menu here */}
+  </>
+);
+
+const swaggerItem = (
+  <MenuItem icon="book" to="/admin/docs">
+    API
+  </MenuItem>
+);
+
+export const AdminMenu = ({ showSwagger }) => (
+  <NavDropdown icon="user-plus" name="Administration" style={{ width: '140%' }} id="admin-menu">
+    {adminMenuItems}
+    {showSwagger && swaggerItem}
+  </NavDropdown>
+);
+
+export default AdminMenu;
